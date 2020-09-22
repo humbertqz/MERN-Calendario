086065f35c2cbef4b0f4d27f10001984c3f6620c
humberto quezada <humberto.shot@gmail.com>
2020-09-22 09:23:23 -0500

    login data

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -9,8 +9,8 @@ export const LoginScreen = () => {
 	const dispatch = useDispatch();
 
 	const [formLoginValues, handleLoginInputChange] = useForm({
-		lEmail: 'user@mail.com',
-		lPassword: '123456',
+		lEmail: '',
+		lPassword: '',
 	});
 
 	const { lEmail, lPassword } = formLoginValues;
@@ -21,10 +21,10 @@ export const LoginScreen = () => {
 	};
 
 	const [formRegisterValues, handleRegisterInputChange] = useForm({
-		rName: 'Beto',
-		rEmail: 'beto@mail.com',
-		rPassword1: '123456',
-		rPassword2: '123456',
+		rName: '',
+		rEmail: '',
+		rPassword1: '',
+		rPassword2: '',
 	});
 
 	const { rName, rEmail, rPassword1, rPassword2 } = formRegisterValues;
